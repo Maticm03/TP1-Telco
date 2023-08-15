201ce873f91190473f10a5d003d79cf2a709f3d9
Matías Cobos <maticobos2003@gmail.com>
2023-08-14 22:49:59 -0300

    Link y Quality
    Funciones de los módulos Link y Quality

--- Quality.hs
+++ Quality.hs
@@ -4,10 +4,10 @@ module Quality ( Quality, newQ, capacityQ, delayQ )
 data Quality = Qua String Int Float deriving (Eq, Show)
 
 newQ :: String -> Int -> Float -> Quality
-
+newQ quality capacidad delay = Qua quality capacidad delay
 
 capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
-
+capacityQ (Qua _ capacidad _) = capacidad
 
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
-
+delayQ (Qua _ _ delay) = delay
