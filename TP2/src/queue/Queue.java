3762e68f5cb76c695b153f07a5bb891feaa17731
Matías Cobos <maticobos2003@gmail.com>
2023-09-11 13:14:17 -0300

    Update Queue.java

--- TP2/src/queue/Queue.java
+++ TP2/src/queue/Queue.java
@@ -4,30 +4,40 @@ import java.util.LinkedList;
 
 public class Queue {
   private LinkedList<Object> queue = new LinkedList<>();
+  
   	public boolean isEmpty() {
 	  return queue.isEmpty();
 	}
+  	
 //en isEmpty decía que tenía que devolver True.
+  	
 	public Queue add( Object  cargo ) {
 	  queue.add(cargo);
 		return this;
 	}
 
 	public Object take() {
-	  if (isEmpty()) {
-		throw new IllegalStateException("La cola está vacía");
+	    if (isEmpty()) {
+	        throw new Error("Queue is empty");
 	    }
-	    queue.removeFirst(); // Elimina el elemento del frente de la cola
-	    return this;
-	}
-//take devolvía null.
-	public Object head() {
-	  
-		return null;
+	    
+	    Object removedItem = queue.removeFirst(); // Elimina y almacena el elemento del frente de la cola
+	    return removedItem; // Devuelve el elemento eliminado
 	}
 
-	public int size() {
-	  
-		return 0;
+//take devolvía null.
+	
+	public Object head() {
+	    if (isEmpty()) {
+	        throw new Error("Queue is empty");
+	    }
+	    
+	    return queue.getFirst();
 	}
+	
+	public int size() {
+	    return queue.size(); // Devuelve el tamaño de la cola
+	}
+
+
 }
