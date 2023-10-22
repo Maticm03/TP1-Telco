7e66ab3b655df43bd029296d80a9e2bad024af4b
Matías Cobos <maticobos2003@gmail.com>
2023-10-22 19:42:21 -0300

    Update NemoTest.java

--- TP3/NemoTest.java
+++ TP3/NemoTest.java
@@ -8,33 +8,30 @@ public class NemoTest {
 
 	@Test void test01NewNemoPosition () {
 		Nemo nemo = new Nemo();
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
-		assertEquals(0,nemo.getDepth());
+		nemoCoordinates(nemo, 0, 0, 0);
+
+
+//		assertEquals(0,nemo.getPositionX());
+//		assertEquals(0,nemo.getPositionY());
+//		assertEquals(0,nemo.getDepth());
 	}
-	
+
 	@Test void test02NemoCanMoveForward() {
 		Nemo nemo = new Nemo();
 		nemo.move("f");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(1,nemo.getPositionY());
-		assertEquals(0,nemo.getDepth());
+		nemoCoordinates(nemo, 0, 1, 0);
 	}
 	
 	@Test void test03NemoCanMoveDown() {
 		Nemo nemo = new Nemo();
 		nemo.move("d");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
-		assertEquals(1,nemo.getDepth());
+		nemoCoordinates(nemo, 0, 0, 1);
 	}
 	
 	@Test void test04NemoCanMoveUp() {
 		Nemo nemo = new Nemo();
 		nemo.move("du");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
-		assertEquals(0,nemo.getDepth());
+		nemoCoordinates(nemo, 0, 0, 0);
 	}
 	
 	@Test void test05NemoCanRotateLeft() {
@@ -52,27 +49,24 @@ public class NemoTest {
 	@Test void test07NemoCanReleaseCapsuleOnSurface() {
 		Nemo nemo = new Nemo();
 		nemo.move("m");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getDepth());
+		nemoCoordinates(nemo, 0, 0, 0);
 	}
 	
 	@Test void test08NemoCanReleaseCapsuleOnFirstLevelOfInmersion() {
 		Nemo nemo = new Nemo();
 		nemo.move("dm");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(1,nemo.getDepth());
+		nemoCoordinates(nemo, 0, 0, 1);
 	}
 	
 	@Test void test09NemoCannotReleaseCapsuleWhenDeeperThanFirstLevelOfInmersion() {
 		Nemo nemo = new Nemo();
 		nemo.move("dd");
-		try {
-			nemo.move("m");
-		} catch (Error e) {
-			assertEquals (e.getMessage(), "Nemo has exploded");
-		}
+		assertThrowsLike( () -> nemo.move("m"), "Nemo has exploded" );
+//		try {
+//			nemo.move("m");
+//				} catch (Error e) {
+//			assertEquals (e.getMessage(), "Nemo has exploded");
+//		}
 		
 	}
 	
@@ -85,87 +79,84 @@ public class NemoTest {
 	@Test void test11NemoCanRotateRightAndMoveForward() {
 		Nemo nemo = new Nemo();
 		nemo.move("rf");
-		assertEquals(nemo.getDirection().strDirection(), "East");
-		assertEquals(1,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
+		XYAndDirection(nemo, 1, 0, "East");
 	}
-	
+
 	@Test void test12NemoCanRotateLeftAndMoveForward() {
 		Nemo nemo = new Nemo();
 		nemo.move("lf");
-		assertEquals(nemo.getDirection().strDirection(), "West");
-		assertEquals(-1,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
+		XYAndDirection(nemo, -1, 0, "West");
 	}
 	
 	@Test void test13TurningRightFourTimesReturnsNemoToOriginalDirection() {
 		Nemo nemo = new Nemo();
 		nemo.move("rrrr");
-		assertEquals(nemo.getDirection().strDirection(), "North");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
+		XYAndDirection(nemo, 0, 0, "North");
 	}
 	
 	@Test void test14TurningLeftFourTimesReturnsNemoToOriginalDirection() {
 		Nemo nemo = new Nemo();
 		nemo.move("llll");
-		assertEquals(nemo.getDirection().strDirection(), "North");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(0,nemo.getPositionY());
+		XYAndDirection(nemo, 0, 0, "North");
 	}
 
 	@Test void test15ComplexDirectives() {
 		Nemo nemo = new Nemo();
 		nemo.move("lflf");
-		assertEquals(nemo.getDirection().strDirection(), "South");
-		assertEquals(-1,nemo.getPositionX());
-		assertEquals(-1,nemo.getPositionY());
+		XYAndDirection(nemo, -1, -1, "South");
 		
 	}
 	
 	@Test void test16ComplexDirectives() {
 		Nemo nemo = new Nemo();
 		nemo.move("ffdmr");
-		assertEquals(nemo.getDirection().strDirection(), "East");
-		assertEquals(0,nemo.getPositionX());
-		assertEquals(2,nemo.getPositionY());
-		assertEquals(1,nemo.getDepth());
+		nemoCoodinatesAndDirection(nemo, 0, 2, 1, "East");
 	}
 	
 	@Test void test17ComplexDirectives() {
 		Nemo nemo = new Nemo();
 		nemo.move("ddlfrfu");
-		assertEquals(nemo.getDirection().strDirection(), "North");
-		assertEquals(-1,nemo.getPositionX());
-		assertEquals(1,nemo.getPositionY());
-		assertEquals(1,nemo.getDepth());
+		nemoCoodinatesAndDirection(nemo, -1, 1, 1, "North");
 	}
 
 	@Test void test18ComplexDirectives() {
 		Nemo nemo = new Nemo();
 		nemo.move("mrrffdrf");
-		assertEquals(nemo.getDirection().strDirection(), "West");
-		assertEquals(-1,nemo.getPositionX());
-		assertEquals(-2,nemo.getPositionY());
-		assertEquals(1,nemo.getDepth());
+		nemoCoodinatesAndDirection(nemo, -1, -2, 1, "West");
 	}
 	
 	@Test void test19ComplexDirectives() {
 		Nemo nemo = new Nemo();
 		nemo.move("llfdrffdfl");
-		assertEquals(nemo.getDirection().strDirection(), "South");
-		assertEquals(-3,nemo.getPositionX());
-		assertEquals(-1,nemo.getPositionY());
-		assertEquals(2,nemo.getDepth());
+		nemoCoodinatesAndDirection(nemo, -3, -1, 2, "South");
 	}
 	
 	@Test void test20ComplexDirectives() {
 		Nemo nemo = new Nemo();
 		nemo.move("lffmddrrfulfdrrffuumu");
-		assertEquals(nemo.getDirection().strDirection(), "South");
-		assertEquals(-1,nemo.getPositionX());
-		assertEquals(-1,nemo.getPositionY());
-		assertEquals(0,nemo.getDepth());
+		nemoCoodinatesAndDirection(nemo, -1, -1, 0, "South");
+	}
+
+	private static void nemoCoodinatesAndDirection(Nemo nemo, int positionX, int positionY, int positionDepth, String direction) {
+		assertEquals(nemo.getDirection().strDirection(), direction);
+		nemoCoordinates(nemo, positionX, positionY, positionDepth);
+	}
+
+	private void assertThrowsLike(Executable executable, String message) {
+		assertEquals(message,
+				Assertions.assertThrows( RuntimeException.class, executable ).getMessage() );
+	}
+
+	private static void nemoCoordinates(Nemo nemo, int positionX, int positionY, int positionDepth) {
+		assertEquals(positionX, nemo.getPositionX());
+		assertEquals(positionY, nemo.getPositionY());
+		assertEquals(positionDepth, nemo.getDepth());
+	}
+
+	private static void XYAndDirection(Nemo nemo, int positionX, int positionY, String direction) {
+		assertEquals(nemo.getDirection().strDirection(), direction);
+		assertEquals(positionX, nemo.getPositionX());
+		assertEquals(positionY, nemo.getPositionY());
 	}
 	
 }
