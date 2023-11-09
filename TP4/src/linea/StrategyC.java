08006151da2e66fc43c695bb27e4c97257bfd4b6
Matías Cobos <maticobos2003@gmail.com>
2023-11-09 18:02:50 -0300

    Update files

--- TP4/src/linea/StrategyC.java
+++ TP4/src/linea/StrategyC.java
@@ -9,22 +9,16 @@ public class StrategyC extends StrategyA {
 
     @Override
     protected boolean checkHorizontalWin(char[][] gameBoard, int row, char player) {
-        // Implementa la lógica específica para la estrategia C en horizontal
-        // Puedes usar la implementación de StrategyA si es aplicable
         return super.checkHorizontalWin(gameBoard, row, player);
     }
 
     @Override
     protected boolean checkVerticalWin(char[][] gameBoard, int column, char player) {
-        // Implementa la lógica específica para la estrategia C en vertical
-        // Puedes usar la implementación de StrategyB si es aplicable
         return super.checkVerticalWin(gameBoard, column, player);
     }
 
     @Override
     protected boolean checkDiagonalWin(char[][] gameBoard, char player) {
-        // Implementa la lógica específica para la estrategia C en diagonal
-        // Puedes usar la implementación de StrategyA o StrategyB si es aplicable
-        return StrategyB.checkDiagonalWin(gameBoard, player) || StrategyB.checkDiagonalWin(gameBoard, player);
+        return super.checkDiagonalWin(gameBoard, player);
     }
 }
