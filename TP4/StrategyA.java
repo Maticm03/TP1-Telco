7431c8528b808585942a875ab6704c2d1abccb02
luciadefrancesco <132778429+luciadefrancesco@users.noreply.github.com>
2023-11-11 13:36:05 -0300

    Update StrategyA.java

--- TP4/StrategyA.java
+++ TP4/StrategyA.java
@@ -6,30 +6,18 @@ public class StrategyA extends WinStrategy {
 	    }
 
 	    private boolean checkHorizontal(Line line, char player) {
-	        for (int i = 0; i < line.getHeight(); i++) {
-	            for (int j = 0; j <= line.getBase() - 4; j++) {
-	                if (line.getBoard().get(i).get(j) == player &&
-	                    line.getBoard().get(i).get(j) == line.getBoard().get(i).get(j + 1) &&
-	                    line.getBoard().get(i).get(j) == line.getBoard().get(i).get(j + 2) &&
-	                    line.getBoard().get(i).get(j) == line.getBoard().get(i).get(j + 3)) {
-	                    return true; // Found 4 in a row horizontally
-	                }
-	            }
-	        }
-	        return false;
+	    	int requiredMatches = 4;
+			return IntStream.range(0, line.getHeight())
+					.anyMatch(i -> IntStream.range(0, line.getBase() - requiredMatches + 1)
+							.anyMatch(j -> IntStream.range(0, requiredMatches)
+									.allMatch(k -> line.getBoard().get(i).get(j + k) == player)));
 	    }
 
 	    private boolean checkVertical(Line line, char player) {
-	        for (int i = 0; i <= line.getHeight() - 4; i++) {
-	            for (int j = 0; j < line.getBase(); j++) {
-	                if (line.getBoard().get(i).get(j) == player &&
-	                    line.getBoard().get(i).get(j) == line.getBoard().get(i + 1).get(j) &&
-	                    line.getBoard().get(i).get(j) == line.getBoard().get(i + 2).get(j) &&
-	                    line.getBoard().get(i).get(j) == line.getBoard().get(i + 3).get(j)) {
-	                    return true; // Found 4 in a row vertically
-	                }
-	            }
-	        }
-	        return false;
-	    }
+	    	int requiredMatches = 4;
+			return IntStream.range(0, line.getHeight() - requiredMatches + 1)
+					.anyMatch(i -> IntStream.range(0, line.getBase())
+							.anyMatch(j -> IntStream.range(0, requiredMatches)
+									.allMatch(k -> line.getBoard().get(i + k).get(j) == player)));
+		}
 }
